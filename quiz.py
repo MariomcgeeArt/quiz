e5331165403f0b91d4589437a0ab9e6373d489ea
Mario McGee <mario.mcgee@students.makeschool.com>
2019-11-20 02:46:43 -0800

    added multiple alpha questions

--- quiz.py
+++ quiz.py
@@ -4,53 +4,90 @@ print("Youll will have 6 question...answer correctly to move on to the next leve
 
 
 
+def state():
+    print("    What is the a rough estimate of the population of New York City? ")
+    print("a) 1 million")
+    print("b) 3 million")
+    print("c) 5 million")
+    print("d) 8 million")
+    print("e) 10 million")
+    answer = input("Please enter a character a-e: ")
+    correct_answer = 'd'
 
-
-What is the a rough estimate of the population of New York City? Please enter a character a-e.
-a) 1 million
-
-b) 3 million
-
-c) 5 million
-
-d) 8 million
-
-e) 10 million
-
-
-
-
-
-
-
-
-
-
-Alaska1 = 0
-
-Alaska2 = 1
-
-
-def Union():
-    '''ask a true or false question!'''
-    answer = int(input("Enter a 0 for false or a 1 for True to the question: "))
-    correct_answer = 0
     if answer == correct_answer:
         print("Correct!")
-        return 1    
+        return 1
     elif answer != correct_answer:
         print("Incorrect!")
-        return 0 
+        return 0
+    
+def rick():
+    print("How old is Rick Sanchez?")
+    print("a)75 ")
+    print("b)70 ")
+    print("c)60 ")
+    print("d)68 ")
+    print("e)timeless ")
+    answer = input("Please enter a character a-e: ")
+    correct_answer = 'b'
+
+    if answer == correct_answer:
+        print("Correct!")
+        return 1
+    elif answer != correct_answer:
+        print("Incorrect!")
+        return 0
+    
+
+
+
+
+# 
+ 
+# 
+
+# 
+# ")
+#    
 
 
 
 
 
-total _score = 0
-result = function()
-total_score += result
-print(total_score)
-`   `
+
+
+
+
+
+# Alaska1 = 0
+
+# Alaska2 = 1
+
+
+# def Union():
+#     '''ask a true or false question!'''
+#     answer = int(input("Enter a 0 for false or a 1 for True to the question: "))
+#     correct_answer = 0
+#     if answer == correct_answer:
+#         print("Correct!")
+#         return 1    
+#     elif answer != correct_answer:
+#         print("Incorrect!")
+#         return 0 
+
+
+state()
+print("")
+print("")
+print("")
+rick()
+
+
+# total _score = 0
+# result = function()
+# total_score += result
+# print(total_score)
+# `   `
 
 
 
