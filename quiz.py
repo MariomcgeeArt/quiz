70757d8da5ccedfc7614fae2cfa3d73bae67e1ad
Mario McGee <mario.mcgee@students.makeschool.com>
2019-11-20 02:14:27 -0800

    first commit

---
+++ quiz.py
@@ -0,0 +1,57 @@
+print("Welcome to the Quiz")
+print("Youll will have 6 question...answer correctly to move on to the next level!")
+
+
+
+
+
+
+What is the a rough estimate of the population of New York City? Please enter a character a-e.
+a) 1 million
+
+b) 3 million
+
+c) 5 million
+
+d) 8 million
+
+e) 10 million
+
+
+
+
+
+
+
+
+
+
+Alaska1 = 0
+
+Alaska2 = 1
+
+
+def Union():
+    '''ask a true or false question!'''
+    answer = int(input("Enter a 0 for false or a 1 for True to the question: "))
+    correct_answer = 0
+    if answer == correct_answer:
+        print("Correct!")
+        return 1    
+    elif answer != correct_answer:
+        print("Incorrect!")
+        return 0 
+
+
+
+
+
+total _score = 0
+result = function()
+total_score += result
+print(total_score)
+`   `
+
+
+
+
